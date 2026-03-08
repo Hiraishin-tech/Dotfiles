b620efe6d7a844418d331340087e624c59455b60
Tomas Pham <tomas@xxx.com>
2026-03-08 19:15:17 +0100

    Added in the git script if there are no changes then the script will end

--- addCommitPush.sh
+++ addCommitPush.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 git status
+if [[ -z "$(git status --short)" ]]; then
+    echo "No changes to commit and push"
+    exit 1
+fi
+
 read -ep "Write a commit message: " message
 if [[ -z "$message" ]]; then
     echo "You must write a message to the commit!!"
